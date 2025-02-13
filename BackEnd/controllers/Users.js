003065a59fea0629184347fb2b668d19e6f2db30
samsul maarif <samsulmaarif1076@gmail.com>
2025-02-13 16:06:48 +0700

    feat: create JWT

--- BackEnd/controllers/Users.js
+++ BackEnd/controllers/Users.js
@@ -1,10 +1,11 @@
 const bcrypt = require('bcryptjs');
-const asyncHandler = require('express');
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
 const User = require('../models/User');
 
 const usersCtrl = {
     // register
-    register: async (req, res) => {
+    register: asyncHandler( async(req, res) => {
         const { username, email, password } = req.body;
         // const user = await User
         // validate
@@ -33,7 +34,34 @@ const usersCtrl = {
             password: userCreated.password,
             id: userCreated._id,
         });
-    },
+    }),
+    login: asyncHandler(async(req, res)=>{
+        const {email, password} = req.body
+        // !Check if user email exists
+        const user = await User.findOne({email});
+        if(!user){
+            throw new Error('Invalid email or password')
+        }
+        // !Check if user password valid
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            res.status(410);
+            throw new Error('Password incorrect');
+        }
+        // !Generate token
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '30d'});
+        // !Send response
+        res.json({
+            message:"Login Succes",
+            token,
+            // username: user.name,
+            // role: user.role,
+            email: user.email,
+            // password: user.password,
+            id: user._id,
+        });
+    })
 };
+
 
 module.exports = usersCtrl;
--- BackEnd/routes/usersRouter.js
+++ BackEnd/routes/usersRouter.js
@@ -3,5 +3,6 @@ const usersRouter = express.Router();
 const usersCtrl = require('../controllers/Users');
 
 usersRouter.post('/api/v1/users/register', usersCtrl.register);
+usersRouter.post('/api/v1/users/login', usersCtrl.login);
 
 module.exports = usersRouter
